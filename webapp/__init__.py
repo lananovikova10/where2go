4ff7047eace9ad16480b2a99aa8039964b9919fc
Polina Doos <doos@list.ru>
2021-10-01 20:29:46 +0300

    выпадающий список

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
+
 def display():
     title = 'Куда поехать из России'
-    return render_template('index.html', page_title = title)
+    country_name = ['Австралия', 'Австрия', 'Азербайджан', 'Албания', 'Алжир', 'Ангола', 'Андорра', 'Антигуа и Барбуда']
+    return render_template('index.html', page_title = title, country_name = country_name)
 
 if __name__=="__main__":
     app.run(debug=True)
