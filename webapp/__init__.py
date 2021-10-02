346e226cec58d2cd7ae6b47d39de76300640bc56
Lana Novikova <svetlnovikova@gmail.com>
2021-10-02 11:00:30 +0300

    added login form and requirements.txt

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from webapp.config import Config
+from webapp.forms import LoginForm
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def display():
     title = 'Куда поехать из России'
     return render_template('index.html', page_title = title)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+
 from db import Base, engine
 
 class Country(Base):
