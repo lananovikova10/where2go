cece615516c6ebc1890a42e2b8386b52189da51c
Lana Novikova <svetlnovikova@gmail.com>
2021-10-02 21:50:25 +0300

    db migration

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, migrate
 
 from webapp.config import Config
 from webapp.forms import LoginForm
 
-
 db = SQLAlchemy()  # creating db instance
 app = Flask(__name__)
+migrate = Migrate()
 app.config.from_object(Config)
 
-def create_app():
-    db.init_app(app)  #  bounding app and bd
-    migrate = Migrate(app, db)  # bounfing app, bd and migration instance
-
-    return app
+db.init_app(app)  #  bounding app and bd
+migrate.init_app(app, db)  # bounfing app, bd and migration instance
 
 from webapp import routes, model
--- webapp/model.py
+++ webapp/model.py
@@ -8,9 +8,14 @@ class Country(db.Model):
     country_name = db.Column(db.String)
 
     def __repr__(self):
-        return f'<User {self.name} {self.email}>'
-
+        return f'<Country {self.country_code} {self.country_name}>'
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'<User {self.login} {self.email}>'
