fc1a233aeadd86a8e00c9b7bc4b5a89aaccbb7b5
Polina Doos <87525081+PolinaDoos@users.noreply.github.com>
2021-10-13 13:36:47 +0300

    func get_info_rosturizm

--- webapp/countries_rosturizm.py
+++ webapp/countries_rosturizm.py
@@ -1,11 +1,17 @@
-import requests
+import requests, pprint, string
 from bs4 import BeautifulSoup
 
-def get_avia_rosturizm():
+# получить страну с формы. Например, из БД по последнему запросу от этого пользователя
+country_arr = 'Грузия'
+
+def get_info_rosturizm(country_arr):
     url = "https://city.russia.travel/safety/kakie_strany_otkryty/"
     html = get_html(url)
-    data = parsed_countries_avia(html)
-    return data
+    if html:
+        data = parse_conditions_rosturizm(html, country_arr)
+        return data
+    else: 
+        return None
 
 
 def get_html(url):
@@ -14,19 +20,33 @@ def get_html(url):
         result.raise_for_status()
         return result.text
     except(requests.RequestException, ValueError):
-        print('Сетевая ошибка')
         return False
 
 
-def parsed_countries_avia(html):
+def parse_conditions_rosturizm(html, country_arr):
     soup = BeautifulSoup(html, 'html.parser')
-    data_list = soup.findAll('div', class_='t537__wrapperleft')
-    countries_avia = []
-    avias = []
-    for item in data_list:
-        country = item.find('div', class_='t537__persname t-name t-name_lg t537__bottommargin_sm').find('a').text
-        countries_avia.append(country)
-        avia = item.find('div', class_='t537__persdescr t-descr t-descr_xxs t537__bottommargin_lg').text
-        avias.append(avia)
+    all_published_country = soup.findAll('div', class_='t336__title t-title t-title_md', field="title")
+    for item in all_published_country:
+        if item.text == country_arr:
+            info_block = item.find_next('div', class_='t-text t-text_md')
+            return get_conditions(info_block)
+    return 'Страна закрыта для туристов'
 
-    return dict(zip(countries_avia, avias)) 
+
+def get_conditions(info_block):
+    country_conditions = {}
+    conditions_info = info_block.findAll('strong')
+    for i in conditions_info:
+        if i.text == 'Транспортное сообщение':
+            country_conditions['transportation'] = info_block.text.split('Транспортное сообщение')[1].split('Виза')[0]
+        elif i.text == 'Прямое авиасообщение':
+            country_conditions['transportation'] = i.text
+        country_conditions['visa'] = info_block.text.split('Виза')[1].split('Условия въезда')[0].strip(string.punctuation).strip()
+        country_conditions['conditions'] = info_block.text.split('Условия въезда')[1].split('Какие вакцины признаются')[0].strip(string.punctuation).strip()
+        country_conditions['vaccine'] = info_block.text.split('Какие вакцины признаются')[1].split('Что открыто')[0].strip(string.punctuation).strip()
+        country_conditions['open_objects'] = info_block.text.split('Что открыто')[1].split('Ограничения')[0].strip(string.punctuation).strip()
+        country_conditions['restrictions '] = info_block.text.split('Ограничения')[1].split('Полезные телефоны')[0].strip(string.punctuation).strip()
+    return country_conditions
+
+#убрать, если настроен вывод на шаблон
+pprint.pprint(get_info_rosturizm(country_arr))
