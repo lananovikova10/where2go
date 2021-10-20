717ed2e0170a533fe53eae7fd9ebf6e8edfb5433
Lana Novikova <svetlnovikova@gmail.com>
2021-10-21 00:08:46 +0300

    changed countries_rostutizm function to process all possible data that we can receive

--- webapp/country/views.py
+++ webapp/country/views.py
@@ -7,8 +7,12 @@ from flask_login import current_user, login_required
 from webapp.country.forms import CounryChoose, UserRequest
 from webapp import db
 
+from webapp.countries_rosturizm import get_info_rosturizm
+from webapp import log
+
 
 blueprint = Blueprint('country_related', __name__, url_prefix='/countries')
+
 
 @blueprint.route('/process_country', methods=['GET', 'POST'])
 def check_signin():
@@ -17,6 +21,7 @@ def check_signin():
     else:
         flash('пожалуйста, авторизируйтесь')
         return redirect(url_for('user_related.login'))
+
 
 def process_country():
     form = CounryChoose()
@@ -34,6 +39,7 @@ def process_country():
         flash('одинаковые страны, попробуйте еще')
         return redirect(url_for('main_page.display'))
 
+
 @blueprint.route('/country_request')
 @login_required
 def country_request():
@@ -41,4 +47,35 @@ def country_request():
     que = UserRequest.query.filter(UserRequest.user_id==current_user.id).order_by(UserRequest.id.desc()).limit(1)
     dep = que[0].country_dep
     arr = que[0].country_arr
-    return render_template('country/country_request.html', page_title=title, country_dep=dep, country_arr=arr)
+    restrictions_by_country = get_info_rosturizm(arr)
+    log.logging.info(arr)
+    if restrictions_by_country == {}:
+        no_data_by_country = "Сюда пока нельзя"
+        log.logging.info(restrictions_by_country)
+        return render_template('country/country_request.html', page_title=title, country_dep=dep, country_arr=arr, no_data_by_country = no_data_by_country)
+    else:
+        if 'transportation' in restrictions_by_country:
+            transportation = restrictions_by_country['transportation']
+        else:
+            transportation = "У нас пока нет информации"
+        if 'visa' in restrictions_by_country:
+            visa = restrictions_by_country['visa']
+        else:
+            visa = "У нас пока нет информации"
+        if 'vaccine' in restrictions_by_country:
+            vaccine = restrictions_by_country['vaccine']
+        else:
+            vaccine = "У нас пока нет информации"
+        if 'open_objects' in restrictions_by_country:
+            open_objects = restrictions_by_country['open_objects']
+        else:
+            open_objects = "У нас пока нет информации"
+        if 'conditions' in restrictions_by_country:
+            conditions = restrictions_by_country['conditions']
+        else:
+            conditions = "У нас пока нет информации"    
+        if 'restrictions' in restrictions_by_country:
+            lockdown_restrictions = restrictions_by_country['restrictions']
+        else:
+            lockdown_restrictions = "У нас пока нет информации"
+        return render_template('country/country_request.html', page_title=title, country_dep=dep, country_arr=arr, transportation = transportation, visa = visa, vaccine = vaccine, open_objects = open_objects, conditions = conditions, lockdown_restrictions = lockdown_restrictions)
