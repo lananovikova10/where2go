c5436e77397cce8f63a1063fa02a1a66cdceb18c
Polina Doos <87525081+PolinaDoos@users.noreply.github.com>
2021-10-24 19:50:19 +0300

    delete from db separate from get countries

--- get_countries.py
+++ get_countries.py
@@ -1,7 +1,6 @@
 from webapp import app, db
 from webapp.fetch_countries import parse_country_data
-from webapp.country.models import Country
+
 
 with app.app_context():
-    Country.query.delete()
     parse_country_data()
