0926586fa58f61faec54e5655f47ed6df0c1c27c
Polina Doos <87525081+PolinaDoos@users.noreply.github.com>
2021-10-26 15:19:15 +0300

    first workable

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,10 +1,10 @@
-from flask import render_template, redirect, flash, url_for, Blueprint
+from flask import render_template, redirect, flash, url_for, Blueprint, request
 
 
 from flask_login import login_user, logout_user, current_user, login_required
 
 from webapp import db
-from webapp.country.models import UserRequest
+from webapp.country.models import Country, UserRequest
 from webapp.user.forms import LoginForm, RegistrationForm, User
 
 
@@ -55,9 +55,37 @@ def register():
     return render_template('user/register.html', page_title='Страница регистрации', form=form)
 
 
-@blueprint.route('/requests', methods=['GET'])
+@blueprint.route('/requests')
 @login_required
 def show_user_requests():
-    all_user_requests = UserRequest.query.filter(UserRequest.user_id==current_user.id).order_by(UserRequest.id.desc()).all()
+    all_user_requests = UserRequest.query.filter(UserRequest.user_id==current_user.id).order_by(UserRequest.id.desc()).limit(25)
+    country_to_id_mapping = get_country_id(all_user_requests)
     return render_template('user/user_requests.html', page_title='История ваших запросов',
-                           all_user_requests=all_user_requests, user_name=current_user.login)
+                           all_user_requests=all_user_requests, user_name=current_user.login,
+                           country_to_id_mapping=country_to_id_mapping)
+
+
+def get_country_id(country_list):
+    country_to_id_mapping = {}
+    for country in country_list:
+        country_from_db = Country.query.filter_by(country_name=country.country_arr).first()
+        if country_from_db:
+            country_to_id_mapping[country_from_db.country_name] = country_from_db.id
+        country_from_db = Country.query.filter_by(country_name=country.country_dep).first()
+        if country_from_db:
+            country_to_id_mapping[country_from_db.country_name] = country_from_db.id
+    return(country_to_id_mapping)
+
+
+@blueprint.route('/process_country_from_user_request')
+def process_country_from_user_request():
+    id_arr = int(request.args.get('arr'))
+    id_dep = int(request.args.get('dep'))
+    country_from_db = Country.query.filter_by(id=id_dep).first()
+    select_dep = country_from_db.country_name
+    country_from_db = Country.query.filter_by(id=id_arr).first()
+    select_arr = country_from_db.country_name
+    choice = UserRequest(user_id=current_user.id, country_dep=select_dep, country_arr=select_arr)
+    db.session.add(choice)
+    db.session.commit()
+    return redirect(url_for('country_related.country_request'))
