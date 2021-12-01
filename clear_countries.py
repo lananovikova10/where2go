29689ec48ab720314fdec8fa91019549e8a2d4fa
Polina Doos <87525081+PolinaDoos@users.noreply.github.com>
2021-12-01 14:13:22 +0300

    updated clear country and readme

--- clear_countries.py
+++ clear_countries.py
@@ -2,4 +2,5 @@ from webapp import app, db
 from webapp.country.models import Country
 
 with app.app_context():
-    Country.query.delete()
+    db.session.query(Country).delete()
+    db.session.commit()
